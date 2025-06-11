7fce4e872de1cd32cebbcc4ff9dce294a0d1d329
ColdAsianrice <dphan4206@gmail.com>
2025-06-11 14:00:36 -0500

    Reviewed!

--- review.js
+++ review.js
@@ -1,20 +1,23 @@
 // CODE ALONG, PART 1
 // 1. Take a look at the CSS - notice how parts of the story have been hidden.
-
 // 2. Take look at the HMTL. When the user clicks the "option-one" button, we want the "option-one-screen" div to show. 
 //  - First, store those elements as variables. Can someone take me through the steps to do that?
-
+let optionOneButton = document.querySelector(".option-one");
+let optionOneScreen = document.querySelector(".option-one-screen");
 
 // CODE SOLO, PART 1
 // 3. Declare a variable to save the button with the class "option-two"
 // 4. Declare a variable to save the div with the class "option-two-screen"
-
+let optionTwoButton = document.querySelector(".option-two");
+let optionTwoScreen = document.querySelector(".option-two-screen");
 
 
 // CODE ALONG, PART 2
 // 5. Let's use the console to check our variables saved correctly. 
 //  - Watch as I use console.log() to check the contents of our option-one variables.
-
+console.log(optionOneButton);
+console.log(optionTwoButton);   
+console.log(optionTwoScreen);
 
 // CODE SOLO, PART 2
 // 6. Now use console.log() to check the contents of the option-two variables you created. 
@@ -27,14 +30,12 @@
 // 7. Plug in the variables we created to make it so that parts of the story show up when the user clicks the button! 
 //  - Uncomment the code, try it out on the webpage, and see what happens!
 
-/* Do not worry about the code below until the end! 
+// Do not worry about the code below until the end! 
 
- INSERT VARIABLE FOR OPTION ONE BUTTON.addEventListener("click", function(){
-    INSERT VARIABLE FOR OPTION-ONE-SCREEN DIV.style.display="block";
+optionOneButton.addEventListener("click", function(){
+optionOneScreen.style.display="block";
 });
 
- INSERT VARIABLE FOR OPTION TWO BUTTON.addEventListener("click", function(){
-    INSERT VARIABLE FOR OPTION-TWO-SCREEN DIV.style.display="block";
+optionTwoButton.addEventListener("click", function(){
+ optionTwoScreen.style.display="block";
 });
-
-*/
